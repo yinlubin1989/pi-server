7792a6ff8757005a48df3eefd4ad8b810fbc50f9
阴璐斌 <yinlubin@koolearn-inc.com>
2018-05-29 02:29:53 +0800

    联调

--- config/boot.sh
+++ config/boot.sh
@@ -4,9 +4,9 @@ pm2 start /home/pi/www/pi-server/manage/bin/www
 pm2 start /home/pi/Util/webcam/websocket-relay.js -- 0
 
 
-while sleep 60
+while sleep 10
 do
 
-ffmpeg -f v4l2  -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/0
+ffmpeg -f v4l2  -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/0 &
 
 done
