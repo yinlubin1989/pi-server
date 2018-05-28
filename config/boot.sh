e3dc14f195c8de1764d5276460ceaa9fcbf9e083
阴璐斌 <yinlubin@koolearn-inc.com>
2018-05-29 02:34:00 +0800

    联调

--- config/boot.sh
+++ config/boot.sh
@@ -7,6 +7,6 @@ pm2 start /home/pi/Util/webcam/websocket-relay.js -- 0
 while sleep 10
 do
 
-ffmpeg -f v4l2  -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/0 &
+nohup ffmpeg -f v4l2  -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/0 &
 
 done
